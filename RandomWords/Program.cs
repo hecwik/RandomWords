fb39791b10d63ffae1cad9e7849ed1d832e0eace
Hector Wiklund <heckee@gmail.com>
2022-09-02 02:30:15 +0200

    Added textinfo class

--- RandomWords/Program.cs
+++ RandomWords/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
 using System.Runtime.CompilerServices;
+using System.Globalization;
 
 string text = File.ReadAllText(@"C:\Users\hecke\source\repos\RandomWords\RandomWords\Resources\SvenskaOrd.txt");
 string[] lines = File.ReadAllLines(@"C:\Users\hecke\source\repos\RandomWords\RandomWords\Resources\SvenskaOrd.txt");
@@ -19,6 +19,8 @@ Console.WriteLine("How many words do you want to generate?");
 int.TryParse(Console.ReadLine(), out int nrOfWords);
 Console.WriteLine();
 
+TextInfo myTI = new CultureInfo("sv-SE", false).TextInfo; // used for capitalizing the first word in the sequence.
+
 if (nrOfWords == 1)
 {
     for (int i = 0; i < nrOfWords; i++)
@@ -27,39 +29,41 @@ if (nrOfWords == 1)
 
         for (int j = 0; j < 1; j++)
         {
-            Console.Write($"{lines[randomWordNumber1]}.");
+            Console.WriteLine("{1}", lines[randomWordNumber1], myTI.ToTitleCase(lines[randomWordNumber1]));
+            //Console.Write($"{lines[randomWordNumber1].Replace(lines[randomWordNumber1][0], char.ToUpper(lines[randomWordNumber1][0]))}");
             Console.WriteLine();
         }
     }
 }
-else if (nrOfWords > 1)
+else if (nrOfWords > 1 && nrOfWords < 40000)
 {
-    for (int i = 0; i < nrOfWords - 1; i++)
+    for (int i = 0; i < nrOfWords + 1; i++)
     {
         int randomWordNumber1 = randomWord.Next(0, nrOfLines);
 
         for (int j = 0; j < 1; j++)
         {
-            Console.Write($"{lines[randomWordNumber1]} ");
-            if (i + 1 == nrOfWords + 1)
+            if (i == 0)
             {
-                Console.Write($".");
+                Console.Write("{1} ", lines[randomWordNumber1], myTI.ToTitleCase(lines[randomWordNumber1]));
             }
+            if (i > 0 && i < nrOfWords - 1)
+            {
+                Console.Write($"{lines[randomWordNumber1]} ");
+
+                
+            }
+            if (i == nrOfWords - 1)
+            {
+                Console.Write($"{lines[randomWordNumber1 + 1]}.");
+                Console.WriteLine();
+            }
+
         }
     }
 }
-
-
-
-
-
-/*int randomWordNumber2 = randomWord.Next(0, nrOfLines);
-int randomWordNumber3 = randomWord.Next(0, nrOfLines);
-
-for (int i = 0; i < 1; i++)
+else if (nrOfWords >= 40000)
 {
-    if(lines[randomWordNumber1] != lines[randomWordNumber2] && 
-        lines[randomWordNumber2] != lines[randomWordNumber3] && 
-        lines[randomWordNumber1] != lines[randomWordNumber3])
-    Console.WriteLine($"{lines[randomWordNumber1]} {lines[randomWordNumber2]} {lines[randomWordNumber3]}.");
-}*/
+    Console.WriteLine("Can't make more than 40 000 words! Dont push your machine too much. :)");
+    Console.ReadLine();
+}
